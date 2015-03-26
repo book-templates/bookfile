f68909876e00b1ffc73efb59251de77fb8044e0a
Gerald Bauer <gerald.bauer@gmail.com>
2015-03-26 18:04:15 +0100

    up

--- beer.rb
+++ beer.rb
@@ -1,6 +1,87 @@
 # encoding: utf-8
 
+package 'book-templates/beer'
 
+
+beer adapter: 'sqlite3', database: './beer.db'
+
+
+
+book do |b|
+
+  opts = {}
+
+  ### generate what's news in 2015
+
+  years = [2015,2014,2013,2012,2011,2010]
+  years.each do |year|
+    b.page( "#{year}", title:  "What's News in #{year}?",
+                     id: "#{year}" ) do |page|
+      page.render_whats_news_in_year( year, opts )
+    end
+  end
+
+
+  ### generate breweries index
+
+  b.page( 'breweries', title: 'Breweries Index',
+                       id: 'breweries' ) do |page|
+    page.render_breweries_idx( opts )
+  end
+
+
+  ### generate beers index
+
+  b.page( 'beers', title: 'Beers Index',
+                   id: 'beers' ) do |page|
+    page.render_beers_idx( opts )
+  end
+
+
+  ### generate brands index
+
+  b.page( 'brands', title: 'Brands Index',
+                    id: 'brands' ) do |page|
+    page.render_brands_idx( opts )
+  end
+
+
+  ### generate table of contents (toc)
+
+  b.page( 'index', title: 'Contents',
+                   id: 'index' ) do |page|
+    page.render_toc( opts )
+  end
+
+
+  ### generate pages for countries
+
+  country_count=0
+  # Country.where( "key in ('at','mx','hr', 'de', 'be', 'nl', 'cz')" ).each do |country|
+  Country.order(:id).each do |country|
+    beers_count     = country.beers.count
+    breweries_count = country.breweries.count
+    next if beers_count == 0 && breweries_count == 0
+  
+    country_count += 1
+    puts "build country page #{country.key}..."
+
+    path = country.to_path
+    puts "path=#{path}"
+    b.page( path, title: "#{country.title} (#{country.code})",
+                  id:    "#{country.key}" ) do |page|
+      page.render_country( country, opts )
+    end
+
+    ## todo - add  b.divider() - for inline version - why, why not ????
+
+    ## break if country_count == 3    # note: for testing only build three country pages
+  end
+
+end # book block
+
+
+=begin
 ###
 ## todo: move to its own bookfile
 ##   - how to pass in country parameter ???
@@ -41,79 +122,4 @@ def build_book_for_country( country_code, opts={} )
 
 end
 
-
-
-def build_book( opts={} )
-
-b = BookBuilder.new( PAGES_DIR, opts )
-
-### generate what's news in 2014
-
-years = [2014,2013,2012,2011,2010]
-years.each do |year|
-  b.page( "#{year}", title:  "What's News in #{year}?",
-                     id: "#{year}" ) do |page|
-    page.write render_whats_news_in_year( year, opts )
-  end
-end
-
-
-### generate breweries index
-
-b.page( 'breweries', title: 'Breweries Index',
-                     id: 'breweries' ) do |page|
-  page.write render_breweries_idx( opts )
-end
-
-
-### generate beers index
-
-b.page( 'beers', title: 'Beers Index',
-                 id: 'beers' ) do |page|
-  page.write render_beers_idx( opts )
-end
-
-
-### generate brands index
-
-b.page( 'brands', title: 'Brands Index',
-                  id: 'brands' ) do |page|
-  page.write render_brands_idx( opts )
-end
-
-
-### generate table of contents (toc)
-
-b.page( 'index', title: 'Contents',
-                 id: 'index' ) do |page|
-  page.write render_toc( opts )
-end
-
-
-
-### generate pages for countries
-
-country_count=0
-# Country.where( "key in ('at','mx','hr', 'de', 'be', 'nl', 'cz')" ).each do |country|
-Country.order(:id).each do |country|
-  beers_count     = country.beers.count
-  breweries_count = country.breweries.count
-  next if beers_count == 0 && breweries_count == 0
-  
-  country_count += 1
-  puts "build country page #{country.key}..."
-
-  path = country.to_path
-  puts "path=#{path}"
-  b.page( path, title: "#{country.title} (#{country.code})",
-                id:    "#{country.key}" ) do |page|
-    page.write render_country( country, opts )
-  end
-
-  ## todo - add  b.divider() - for inline version - why, why not ????
-
-  ## break if country_count == 3    # note: for testing only build three country pages
-end
-
-end # method build_book
-
+=end
--- world.rb
+++ world.rb
@@ -3,44 +3,30 @@ package 'book-templates/world'
 
 
 world adapter: 'sqlite3', database: './world.db'
-## will connect to database;
-## will require all libs
-## will include all models
 
-
-puts "self.class.name (in top level): #{self.class.name}"
 
 book do |b|
- 
-  puts "before first page"
-  puts "self.class.name (in book block): #{self.class.name}"
- 
+
+  opts = {}
+
   b.page 'index', title: 'Contents',
-                  id:    'index'   do |p|
-    puts "enter index"
-    
-    p.render_toc( {} )
-    ## render :toc   -- auto includes opts???
-
-    puts "self.class.name (in page block): #{self.class.name}"
-    puts "leave index"
- end
-
+                  id:    'index'   do |page|
+    page.render_toc( opts )  ### fix: auto-include opts in render - how??
+  end
 
   ### generate pages for countries
   # note: use same order as table of contents
 
   Continent.order(:id).each do |continent|
-    continent.countries.order(:name).limit(1).each do |country|
+    continent.countries.order(:name).each do |country|
 
       puts "build country page #{country.key}..."
       path = country.to_path
       puts "path=#{path}"
-    
+
       b.page path, title: "#{country.name} (#{country.code})",
-               id:    country.key do |p|
-        p.write "render_country(#{country.name})"  ## render_country( country )  ## render_country( country, opts )
-        p.render_country( country, {} )  ### fix: auto-include opts in render - how??
+                   id:    country.key do |page|
+        page.render_country( country, opts )  ### fix: auto-include opts in render - how??
       end
     end
   end
